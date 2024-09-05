a747b48b99377fc21c647e31d7bdb52e8ab30af9
Ajay Wankhade <ajaysw45@gmail.com>
2024-09-05 13:57:31 +0530

    Completed User operations

---
+++ src/main/java/com/ajaysw/repo/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ajaysw.repo;
+
+import com.ajaysw.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+    User findByUserName(String userName);
+}
---
+++ src/main/java/com/ajaysw/service/UserService.java
@@ -0,0 +1,38 @@
+package com.ajaysw.service;
+
+import com.ajaysw.entity.User;
+import com.ajaysw.repo.UserRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(ObjectId id) {
+        return userRepository.findById(id);
+    }
+
+    public void deleteUser(ObjectId id) {
+        userRepository.deleteById(id);
+    }
+
+    public User findByUserName(String userName) {
+        return  userRepository.findByUserName(userName);
+    }
+}
