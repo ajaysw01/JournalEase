056fb07e8f0f71a347fe7cff73f5d00b4446258f
Ajay Wankhade <ajaysw45@gmail.com>
2024-09-06 15:16:26 +0530

    Done with journal entry and user

--- src/main/java/com/ajaysw/JournalEaseApplication.java
+++ src/main/java/com/ajaysw/JournalEaseApplication.java
@@ -2,11 +2,22 @@ package com.ajaysw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class JournalEaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(JournalEaseApplication.class, args);
+
     }
 
+    @Bean
+    public PlatformTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
+        return new MongoTransactionManager(dbFactory);
+    }
 }
--- src/main/java/com/ajaysw/service/JournalEntryService.java
+++ src/main/java/com/ajaysw/service/JournalEntryService.java
@@ -6,6 +6,7 @@ import com.ajaysw.repo.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,12 +20,18 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName){
-        User user = userService.findByUserName(userName);
-        journalEntry.setDate(LocalDateTime.now());
-        JournalEntry saved = journalEntryRepository.save(journalEntry);
-        user.getJournalEntries().add(saved);
-        userService.saveUser(user);
+        try{
+            User user = userService.findByUserName(userName);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+            userService.saveUser(user);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            throw  new RuntimeException("An error occurred while saving journal entry");
+        }
     }
     public void saveEntry(JournalEntry journalEntry){
         journalEntryRepository.save(journalEntry);
